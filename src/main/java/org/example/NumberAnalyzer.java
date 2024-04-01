9f4a8ec91aec3284c651db0f30582d16487b48f3
marietastepanyan <mar.stepanyannn@gmail.com>
2024-04-01 11:35:47 +0400

    initial commit

---
+++ src/main/java/org/example/NumberAnalyzer.java
@@ -0,0 +1,66 @@
+package org.example;
+
+public class NumberAnalyzer {
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean isPerfectNumber(int num) {
+        if (num <= 0) {
+            return false;
+        }
+
+        int sum = 0;
+
+        for (int i = 1; i <= num / 2; i++) {
+            if (num % i == 0) {
+                sum += i;
+            }
+        }
+
+        return sum == num;
+    }
+
+    public static int gcd(int a, int b) {
+        if (a == 0 || b == 0) {
+            return a + b;
+        }
+
+        a = Math.abs(a);
+        b = Math.abs(b);
+
+        while (b != 0) {
+            if (a > b) {
+                a = a - b;
+            } else {
+                b = b - a;
+            }
+        }
+
+        return a;
+    }
+
+    public static int lcm(int a, int b) {
+        if (a == 0 || b == 0) {
+            return 0;
+        }
+
+        int max = Math.max(a, b);
+
+        for (int lcm = max; ; lcm += max) {
+            if (lcm % a == 0 && lcm % b == 0) {
+                return lcm;
+            }
+        }
+    }
+}
---
+++ src/test/java/org/example/NumberAnalyzerTest.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NumberAnalyzerTest {
+
+    @Test
+    void isPrime() {
+    }
+
+    @Test
+    void isPerfectNumber() {
+    }
+
+    @Test
+    void gcd() {
+    }
+
+    @Test
+    void lcm() {
+    }
+}
